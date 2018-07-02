ebaccfd56caecfc8482e85326ee27ae18ccaa5d9
言sir <chaiguanpeng@users.noreply.github.com>
2018-07-02 22:44:34 +0800

    Update 5.文件copy(随读随写).js

--- 5.文件copy(随读随写).js
+++ 5.文件copy(随读随写).js
@@ -1,1 +1,29 @@
-das
+let fs = require('fs');
+function copy(source, target) {
+    let size = 3;
+    let index = 0;
+    let buffer = Buffer.alloc(size);
+    fs.open(source, 'r', function (err, rfd) {
+        if (err) return console.log(err);
+        fs.open(target, 'w', 0o666, function (err, wfd) {
+            function next() {
+                fs.read(rfd, buffer, 0, size, index, function (err, byteRead) {
+                    fs.write(wfd, buffer, 0, byteRead, index, function (err, byteWritten) {
+                        index += byteRead;
+                        console.log(byteWritten);
+                        if (byteWritten) {
+                            next();
+                        }else{
+                            fs.close(rfd,()=>{});
+                            fs.fsync(function(){ //把内存中的内容强制写入后再关闭文件 写入是异步操作
+                                fs.close(wfd, () => { });
+                            })
+                        }
+                    })
+                })
+            }
+            next();
+        })
+    })
+}
+copy('./1.txt', './5.txt')
